6f119ae935bc4773484140b4dde130b336dce78a
RvanB <rvanbronkhorst@gmail.com>
2026-05-10 22:35:47 -0700

    Default to webgpu

--- src/main.js
+++ src/main.js
@@ -1,14 +1,11 @@
 import { App } from "./ui/App.js";
 import { WebGPUSpreadRenderer } from "./rendering/WebGPUSpreadRenderer.js";
 
-const rendererParam = new URL(window.location.href).searchParams.get("renderer");
-const rendererClass = rendererParam === "webgpu" ? WebGPUSpreadRenderer : undefined;
-
 const app = new App(
   document.getElementById("spread-canvas"),
   document.getElementById("overlay-canvas"),
   document.getElementById("page-strip"),
-  { rendererClass }
+  { rendererClass: WebGPUSpreadRenderer }
 );
 
 app.init();
